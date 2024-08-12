5a60ebcc511e46cd6c9b231517ebd6a3dd6b57a0
ritikanegi-dli0593 <ritika.negi@godigit.com>
2024-08-12 18:03:42 +0530

    implemented login and register

---
+++ src/app/register/register.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.scss']
+})
+export class RegisterComponent {
+ options:any=["English","Hindi"]
+
+}
