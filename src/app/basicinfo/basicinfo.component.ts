c80aaa29111ccc36c1903c3b52e90e147c913be6
ritikanegi-dli0593 <ritika.negi@godigit.com>
2024-08-13 22:09:33 +0530

    basic information , username  and set password in single component using ngIf

--- src/app/basicinfo/basicinfo.component.ts
+++ src/app/basicinfo/basicinfo.component.ts
@@ -6,14 +6,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./basicinfo.component.scss']
 })
 export class BasicinfoComponent {
-selectedMonth!: string;
-selectedGender!:string;
-months:string[]=[
-  'January','Febuary','March','April','May','June','July',
-  'August','September','October','November','December'
-];
-genders:string[]=[
-  'Male','Female','Rather not say','Custom'
-]
-
+  selectedMonth!: string;
+  selectedGender!:string;
+  months:string[]=[
+    'January','Febuary','March','April','May','June','July',
+    'August','September','October','November','December'
+  ];
+  genders:string[]=[
+    'Male','Female','Rather not say','Custom'
+  ]
+  classValue='basicinfo';
+  changePage(e:Event){
+    console.log(this.classValue);
+    e.preventDefault();
+    if(this.classValue=='basicinfo')
+    {
+      this.classValue='username';
+    }
+    else
+    if(this.classValue=='username')
+    {
+      this.classValue='password';
+    }
+  }
 }
