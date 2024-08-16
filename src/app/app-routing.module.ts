6425b52daf6fa5918ae78a387d4e2a511e5a1e97
ritikanegi-dli0593 <ritika.negi@godigit.com>
2024-08-16 19:41:31 +0530

    nesting routes added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,9 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { BasicinfoComponent } from './basicinfo/basicinfo.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { HeaderComponent } from './dashboard/header/header.component';
+import { SidebarComponent } from './dashboard/sidebar/sidebar.component';
 
 const routes: Routes = [
   {
@@ -20,6 +23,20 @@ const routes: Routes = [
   {
     path:'basicinfo',
     component:BasicinfoComponent
+  },
+  {
+    path:'dashboard',
+    component:DashboardComponent,
+    children:[
+      {
+        path:'header',
+        component:HeaderComponent
+      },
+      {
+        path:'sidebar',
+        component:SidebarComponent
+      },
+    ]
   }
 ];
 
