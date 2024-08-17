8266cbac1e4c024f46ed733b661b41f1502982cd
ritikanegi-dli0593 <ritika.negi@godigit.com>
2024-08-17 16:48:57 +0530

    created new component to show deleted notes only

---
+++ src/app/dashboard/trash/trash/trash.component.ts
@@ -0,0 +1,33 @@
+import { Component } from '@angular/core';
+import { HttpclientService } from 'src/app/service/httpclient/httpclient.service';
+@Component({
+  selector: 'app-trash',
+  templateUrl: './trash.component.html',
+  styleUrls: ['./trash.component.scss']
+})
+export class TrashComponent {
+
+  notesList!:any[];
+  constructor(private httpClient:HttpclientService){}
+
+  ngOnInit(): void {
+    this.httpClient.getAllNotes().subscribe({
+      next:(res:any)=>{
+        console.log("res",res.data.data);
+        this.notesList = res.data.data;
+        this.filterData();
+      },
+      error:(err)=>{
+        console.log("err",err);
+      }
+    })
+  }
+  filterData(){
+    this.notesList=this.notesList.filter((item)=>{
+       if(item.isDeleted==true)
+      {
+        return item;
+      }
+    })
+  }
+}
