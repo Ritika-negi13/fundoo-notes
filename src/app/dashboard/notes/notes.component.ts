0ec9c485e47f6a415e6be684d319b77a63736a56
ritikanegi-dli0593 <ritika.negi@godigit.com>
2024-08-18 18:51:29 +0530

    added notes

--- src/app/dashboard/notes/notes.component.ts
+++ src/app/dashboard/notes/notes.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { HttpclientService } from 'src/app/service/httpclient/httpclient.service';
 
 @Component({
   selector: 'app-notes',
@@ -6,5 +7,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./notes.component.scss']
 })
 export class NotesComponent {
+  @Output() title=new EventEmitter();
+  notesList!:any[];
+  constructor(private httpClient:HttpclientService){}
 
+  ngOnInit(): void {
+    this.httpClient.getAllNotes().subscribe({
+      next:(res:any)=>{
+        console.log("res",res.data.data);
+        this.notesList = res.data.data;
+        this.filterData();
+      },
+      error:(err)=>{
+        console.log("err",err);
+      }
+    })
+  }
+  filterData(){
+    this.notesList=this.notesList.filter((item)=>{
+       if(item.isArchived==false || item.isDeleted==false)
+      {
+        return item;
+      }
+    })
+  }
 }
