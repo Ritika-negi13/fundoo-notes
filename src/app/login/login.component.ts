fd874e5453e56d2043e128e2a418d4e5c779351e
ritikanegi-dli0593 <ritika.negi@godigit.com>
2024-08-18 18:55:59 +0530

    modified : router link added for dashboard

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,11 +1,12 @@
-import { Component } from '@angular/core';
+import { Component,ViewChild,ElementRef } from '@angular/core';
 import {FormControl, FormGroupDirective, NgForm, Validators} from '@angular/forms';
 import {ErrorStateMatcher} from '@angular/material/core';
+import { Router } from '@angular/router';
 
 export class MyErrorStateMatcher implements ErrorStateMatcher {
   isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
     const isSubmitted = form && form.submitted;
-    return !!(control && control.invalid && (control.dirty || control.touched || isSubmitted));
+    return !!(control && control.invalid && (control.dirty || isSubmitted));
   }
 }
 
@@ -16,14 +17,27 @@ export class MyErrorStateMatcher implements ErrorStateMatcher {
 })
 
 export class LoginComponent {
+  email!:string;
+  constructor(private router:Router){}
   openNewAccount(e : Event){
     e.preventDefault();
     //@ts-ignore
     document.getElementById('newcard').style.display = 'block';
   }
+  @ViewChild('myInput') myInputElement!: ElementRef;
   emailFormControl = new FormControl('', [
     Validators.required,
     Validators.email,
   ]);
   matcher = new MyErrorStateMatcher();
+  ngOnInit(): void {
+    setTimeout(() => {
+      this.myInputElement.nativeElement.focus();
+    }, 1000);
+  }
+
+  openDashboard() {
+    this.router.navigate(['/dashboard']);
+  }
+
 }
