059d91210b85e3a37b290edf2812f53b433faf60
ritikanegi-dli0593 <ritika.negi@godigit.com>
2024-08-21 17:06:08 +0530

    added : add note and update notes

--- src/app/service/httpclient/httpclient.service.ts
+++ src/app/service/httpclient/httpclient.service.ts
@@ -6,20 +6,26 @@ import { HttpClient } from '@angular/common/http';
 })
 export class HttpclientService {
   constructor(private http:HttpClient) { }
-  getAllNotes(){
-    console.log("working")
-    let accesstoken=localStorage.getItem('access_token')
-    console.log(accesstoken)
-    return this.http.get("https://fundoonotes.incubation.bridgelabz.com/api/notes/getNotesList?access_token="+`${accesstoken}`) 
-  }
+  accesstoken=localStorage.getItem('access_token')
   validateLogin(data:any){
     
     return this.http.post("https://fundoonotes.incubation.bridgelabz.com/api/user/login",data)
+  }
+  getname(){
+    return this.http.get("https://fundoonotes.incubation.bridgelabz.com/api/user?access_token="+`${this.accesstoken}`)
+  }
+  getAllNotes(){
+    return this.http.get("https://fundoonotes.incubation.bridgelabz.com/api/notes/getNotesList?access_token="+`${this.accesstoken}`) 
+  }
+  addnewnote(title:any ,description:any){
+    return this.http.post("https://fundoonotes.incubation.bridgelabz.com/api/notes/addNotes?access_token="+`${this.accesstoken}`,{"title":title,"description":description})
+  }
+  editnote(data:any){
+    return this.http.patch("https://fundoonotes.incubation.bridgelabz.com/api/notes/"+`${data.id}` +"?access_token="+`${this.accesstoken}`,data)
   }
   newuser(data:any){
     console.log("working")
     console.log(data)
     return this.http.post("https://fundoonotes.incubation.bridgelabz.com/api/user/userSignUp",data)
-  
   }
 }
