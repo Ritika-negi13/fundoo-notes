6ded668d0ebc326cb1bcbabf4a315288252d8367
ritikanegi-dli0593 <ritika.negi@godigit.com>
2024-08-21 10:47:51 +0530

    modified: add function to send data in data service

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-
+import { DataService } from '../service/userdata/data.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
@@ -7,8 +8,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent {
+  constructor(private data:DataService,private router:Router){}
  options:any=["English","Hindi"]
   firstname!:string;
   lastname!:string;
-  
+  submit(){
+    this.data.getname(this.firstname,this.lastname);
+    this.router.navigate(['/basicinfo'])
+  }
 }
