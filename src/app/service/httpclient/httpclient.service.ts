42d71ff8e62aceb785149d3c380ad0e817dc7a99
ritikanegi-dli0593 <ritika.negi@godigit.com>
2024-08-21 11:11:57 +0530

    modified: add function to post data for new user

--- src/app/service/httpclient/httpclient.service.ts
+++ src/app/service/httpclient/httpclient.service.ts
@@ -8,6 +8,18 @@ export class HttpclientService {
   constructor(private http:HttpClient) { }
   getAllNotes(){
     console.log("working")
-    return this.http.get("https://fundoonotes.incubation.bridgelabz.com/api/notes/getNotesList?access_token=ARODMBkNgPeEIbNCwX0WolphSA4MDUU5joqXifgXtrqf1UU6FuUHHjIdxE8VMN4U") 
+    let accesstoken=localStorage.getItem('access_token')
+    console.log(accesstoken)
+    return this.http.get("https://fundoonotes.incubation.bridgelabz.com/api/notes/getNotesList?access_token="+`${accesstoken}`) 
+  }
+  validateLogin(data:any){
+    
+    return this.http.post("https://fundoonotes.incubation.bridgelabz.com/api/user/login",data)
+  }
+  newuser(data:any){
+    console.log("working")
+    console.log(data)
+    return this.http.post("https://fundoonotes.incubation.bridgelabz.com/api/user/userSignUp",data)
+  
   }
 }
