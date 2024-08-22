c37f38dcb7f058d126691021b62884be69668bba
ritikanegi-dli0593 <ritika.negi@godigit.com>
2024-08-22 09:13:14 +0530

    added :search functionality to serach bar

--- src/app/dashboard/header/header.component.ts
+++ src/app/dashboard/header/header.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { OpensidebarService } from 'src/app/service/opensidebar/opensidebar.service';
+import { Router } from '@angular/router';
+import { SearchService } from 'src/app/service/search/search.service';
 
 @Component({
   selector: 'app-header',
@@ -7,8 +9,10 @@ import { OpensidebarService } from 'src/app/service/opensidebar/opensidebar.serv
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent {
-  constructor(private os:OpensidebarService){}
+  constructor(private os:OpensidebarService,private router:Router,private ss:SearchService){}
   flag='false';
+  search!:string;
+  username = localStorage.getItem("name");
   opensideBar(){
     this.os.updateFlag(this.flag);
     if(this.flag=='true')
@@ -17,5 +21,12 @@ export class HeaderComponent {
     }
     else
       this.flag='true';
+  }
+  logout(){
+    this.router.navigate(['/login']);
+    localStorage.removeItem('access_token');
+  }
+  onSearch(){
+    this.ss.updateSearch(this.search);
   }
 }
